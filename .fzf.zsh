ee0cb88d271b3b8bd8dd317ec0182236623cc18a
Alberto Harres <betoharres@gmail.com>
2022-07-26 03:43:43 -0300

    feat: add .fzf.zsh file

---
+++ .fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */Users/betoharres/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/Users/betoharres/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/Users/betoharres/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/betoharres/.fzf/shell/key-bindings.zsh"
