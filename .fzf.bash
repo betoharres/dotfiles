6e85ed8ee64875ca89868794e41f2f4b42ec2c0a
Alberto Harres <betoharres@gmail.com>
2023-01-24 03:37:07 +0000

    fix: fzf

--- .fzf.bash
+++ .fzf.bash
@@ -1,14 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */home/betoharres/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/betoharres/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/betoharres/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
-
+# ------------ pacman -S fzf
+source "/home/betoharres/.fzf/shell/key-bindings.bash"
--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/betoharres/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/betoharres/.fzf/bin"
+if [[ ! "$PATH" == */home/betoharres/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/betoharres/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/betoharres/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/betoharres/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/betoharres/.fzf/shell/key-bindings.zsh"
+source "/usr/share/fzf/key-bindings.zsh"
